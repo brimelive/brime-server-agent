461ea2a0c31b6054c68cb4281508e0a9d6e2bd22
geekenmd <justin@watsonnetworks.com>
2021-07-25 00:03:22 -0500

    add websocket

--- agent.js
+++ agent.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(express.json());
 const { exec } = require("child_process");
 
+
+
 // Host Uptime
 function hostUptime(){
     var ut_sec = os.uptime();
@@ -75,6 +77,8 @@ app.post('/execute', async function (req, res) {
 
 app.listen(3000)
 
+
+
 const nets = networkInterfaces();
 const results = {};
 
@@ -82,7 +86,7 @@ for (const name of Object.keys(nets)) {
     for (const net of nets[name]) {
         // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
         if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
+            if (!results[name]) { 
                 results[name] = [];
             }
             results[name].push(net.address);
@@ -91,3 +95,42 @@ for (const name of Object.keys(nets)) {
 }
 
 console.log(results);
+
+
+var mqtt = require('mqtt')
+const clientId = os.hostname()
+
+const host = 'ws://129.213.48.167:8083/mqtt'
+
+const options = {
+  keepalive: 60,
+  clientId: clientId,
+  protocolId: 'MQTT',
+  protocolVersion: 4,
+  clean: true,
+  reconnectPeriod: 1000,
+  connectTimeout: 30 * 1000,
+  will: {
+    topic: 'WillMsg',
+    payload: 'Connection Closed abnormally..!',
+    qos: 0,
+    retain: false
+  },
+}
+console.log('Connecting mqtt client')
+const client = mqtt.connect(host, options)
+
+client.on('error', (err) => {
+  console.log('Connection error: ', err)
+  client.end()
+})
+
+client.on('reconnect', () => {
+  console.log('Reconnecting...')
+})
+
+client.on('connect', () => {
+    console.log('Client connected:' + clientId)
+    // Subscribe
+    client.subscribe('agent', { qos: 0 })
+  })
